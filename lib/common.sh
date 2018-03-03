19e6a48c73916ad68005a7100ee6c404e4588b62
noissefnoc <noissefnoc@gmail.com>
2018-03-03 16:41:24 +0900

    add util functions to lib/common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+header() {
+  echo "" || true
+  echo -e "-----> \e[34m$*\033[0m" || true
+  echo "" || true
+}
+
+log() {
+  echo -e "       $*"
+}
+
+debug() {
+
+  if [ "$EMBULK_BUILDPACK_DEBUG" == "true" ] ; then
+    echo "       [DEBUG] $*"
+  fi
+}
